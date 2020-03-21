6146bdf5cb848e2b8a709a723ca1378c3568df25
Egor Naumov <Egor_Nmv@bk.ru>
2020-03-21 19:01:54 +0500

    added fonts and icons

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Alert } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 import { Navbar } from "./src/components/Navbar";
 import { MainScreen } from "./src/screens/MainScreen";
 import { TodoScreen } from "./src/screens/TodoScreen";
 
+async function loadApplication() {
+  await Font.loadAsync({
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf")
+  });
+}
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [todoId, setTodoId] = useState(null);
   const [todos, setTodos] = useState([
     { id: "1", title: "Todo something" },
     { id: "2", title: "Todo something v 2.0" }
   ]);
+
+  if (!appIsReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onError={error => console.log("error", error)}
+        onFinish={() => setAppIsReady(true)}
+      />
+    );
+  }
 
   const addTodo = title => {
     setTodos(prev => [
